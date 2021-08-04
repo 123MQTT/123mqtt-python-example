c6070e6446cf59791ec0e8dd53ef86c75f8b2978
Ekawahyu Susilo <ekawahyu@yahoo.com>
2021-08-04 02:36:18 -0700

    minor fixes

--- test_alice_bob_signed_messaging.py
+++ test_alice_bob_signed_messaging.py
@@ -15,8 +15,8 @@ print('Alice public : ' + alice_vk.to_string().hex())
 # Alice creates signature for the message she is about to send
 message = 'hello Bob!'
 signature = alice_sk.sign(message.encode())
-print('Alice send: ' + message)
-print('Alice signature: ' + signature.hex())
+print('Alice message: ' + message)
+print('Message signature: ' + signature.hex())
 
 # Man in the middle attack, trying to alter the reply
 # Uncomment the line below and Bob can detect that the reply has been altered
@@ -30,7 +30,7 @@ bob_received_signature = signature.hex()
 bob_received_public_key = alice_vk.to_string().hex()
 print('Message exchange, content: ' + bob_received_msg)
 print('Message exchange, signature: ' + bob_received_signature)
-print('Message excahange, public key: ' + bob_received_public_key)
+print('Message exchange, public key: ' + bob_received_public_key)
 
 # Bob verifies if public key is valid
 vk_string = bytearray.fromhex(bob_received_public_key)
@@ -56,7 +56,7 @@ print('Bob public : ' + bob_vk.to_string().hex())
 message = 'hello Alice, good to hear from the real you!'
 signature = bob_sk.sign(message.encode())
 print('Bob reply: ' + message)
-print('Bob signature: ' + signature.hex())
+print('Reply signature: ' + signature.hex())
 
 # Man in the middle attack, trying to alter the reply
 # Uncomment the line below and Alice can detect that the reply has been altered
@@ -70,7 +70,7 @@ alice_received_signature = signature.hex()
 alice_received_public_key = bob_vk.to_string().hex()
 print('Message exchange, content: ' + alice_received_msg)
 print('Message exchange, signature: ' + alice_received_signature)
-print('Message excahange, public key: ' + alice_received_public_key)
+print('Message exchange, public key: ' + alice_received_public_key)
 
 # Alice verifies if public key is valid
 vk_string = bytearray.fromhex(alice_received_public_key)
